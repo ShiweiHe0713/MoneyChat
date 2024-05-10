b5f2cbddc94d172e0f9facacde3ea49cd432dd6b
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-05-10 12:21:14 -0400

    Seperate even_handler class to a separate file

---
+++ event_handler.py
@@ -0,0 +1,30 @@
+from typing_extensions import override
+from openai import AssistantEventHandler
+# Then, we use the `stream` SDK helper 
+# with the `EventHandler` class to create the Run 
+# and stream the response.
+ 
+# First, we create a EventHandler class to define
+# how we want to handle the events in the response stream.
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+      
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+  
+  def on_tool_call_delta(self, delta, snapshot):
+    if delta.type == 'code_interpreter':
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
+ 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+from event_handler import EventHandler
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -14,3 +15,23 @@ assistant = client.beta.assistants.create(
     model="gpt-4-turbo",
 )
 
+thread = client.beta.threads.create()
+
+# `messages`` is an object that has all the messages users sent
+# `message` supports both text and files, the number of messages can be added to Thread is unlimited
+messages = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content="Tell me my total expense of last three months",
+)
+
+# Then, we use the `stream` SDK helper 
+# with the `EventHandler` class to create the Run 
+# and stream the response.
+with client.beta.threads.runs.stream(
+  thread_id=thread.id,
+  assistant_id=assistant.id,
+  instructions="Please address the user as Calvin He. The user has a premium account.",
+  event_handler=EventHandler(),
+) as stream:
+  stream.until_done()
