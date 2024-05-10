34c177fd49feece68a4e4c790f0a10cfd1adf872
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2024-05-10 14:17:22 -0400

    added statements into file batch

--- main.py
+++ main.py
@@ -12,9 +12,44 @@ assistant = client.beta.assistants.create(
     name="MoneyChat",
     instructions="You are a personal finance helper to analyze statments, bills and give suggestions",
     tools=[{"type": "file_search"}],
-    model="gpt-4-turbo",
+    model="gpt-3.5-turbo",
 )
 
+# Create a vector store
+vector_store = client.beta.vector_stores.create(name="Financial Statement")
+
+# Turn the files into a file stream
+directory = "docs/boa/"
+files = os.listdir(directory)
+
+file_streams = []
+for file in files:
+    file_path = os.getcwd() + "/docs/boa/" + file
+    if os.path.exists(file_path):
+        try:
+            file_stream = open(file_path, "rb")
+            file_streams.append(file_stream)
+        except IOError:
+            print(f"Error: Unable to open file '{file}'")
+    else:
+        print(f"Error: File '{file}' not found")
+
+print("Total files processed:", len(file_streams))
+
+# Use the file batch and poll sdk to upload files
+file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+    vector_store_id=vector_store.id,
+    files=file_streams,
+)
+# print(file_batch.status, file_batch.file_counts)
+
+# Update the assistant
+assistant = client.beta.assistants.update(
+    assistant_id=assistant.id,
+    tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+)
+
+# Create a thread
 thread = client.beta.threads.create()
 
 # `messages`` is an object that has all the messages users sent
@@ -22,7 +57,7 @@ thread = client.beta.threads.create()
 messages = client.beta.threads.messages.create(
     thread_id=thread.id,
     role="user",
-    content="Tell me my total expense of last three months",
+    content="What is the biggest purchase within the past 3 month, and validate it",
 )
 
 # Then, we use the `stream` SDK helper 
